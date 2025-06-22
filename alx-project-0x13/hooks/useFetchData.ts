1c0f81f4350e610fd05e4ef5d5e3e3f881b2c0f9
zakariabanid101 <banidzakaria2@gmail.com>
2025-06-22 19:24:18 +0100

    fix prompt error on useFetchDate

--- alx-project-0x13/hooks/useFetchData.ts
+++ alx-project-0x13/hooks/useFetchData.ts
@@ -1,7 +1,9 @@
 import { ImageProps } from "@/interfaces";
 import { useState } from "react";
 
-const useFetchData = <T, R>() => {
+type HasPrompt = { prompt: string }; // minimum shape for R
+
+const useFetchData = <T, R extends HasPrompt>() => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [responseData, setResponseData] = useState<T | null>(null);
   const [error, setError] = useState<string | null>(null);
@@ -19,20 +21,19 @@ const useFetchData = <T, R>() => {
         }
       });
 
-      if (!resp.ok)
-      {
+      if (!resp.ok) {
         throw new Error('Failed to fetch data');
       }
 
-      const result = await resp.json()
-      setResponseData(result)
-      setGeneratedImages((prev) => [...prev, { imageUrl: result?.message, prompt: body?.prompt }])
-        } catch (err) {
-      setError((err as Error).message)
+      const result = await resp.json();
+      setResponseData(result);
+      setGeneratedImages((prev) => [...prev, { imageUrl: result?.message, prompt: body.prompt }]);
+    } catch (err) {
+      setError((err as Error).message);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
   return {
     isLoading,
@@ -40,8 +41,7 @@ const useFetchData = <T, R>() => {
     error,
     fetchData,
     generatedImages
-  }
-}
-
+  };
+};
 
 export default useFetchData;
